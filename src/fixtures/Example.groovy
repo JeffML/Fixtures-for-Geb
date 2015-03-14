df6a6086662278986069ee5b611c090b9e94d5e5
Jeff Lowery <jeff.l@mavericklabel.com>
2015-03-14 15:35:41 -0700

    Outline of fixture completed

--- src/fixtures/Example.groovy
+++ src/fixtures/Example.groovy
@@ -1,28 +1,30 @@
 package fixtures
 
 class Example {
-	static def pages = [
-		"Appointment Cards": [
-			url: "/products/appointment-label-cards.html",
-			title: "Label Cards and Calendar Stickers - Easy & Secure Online Ordering",
-			fixtures: [
-				entry1: [
-					isActive: true,
-					fields: [
-						style: [name: '147[]', value: 7462],	//AC-302
-						proof: [name: '64[]', value: '482'],	//online
-						quantity: [name: 'qty[]', value: '500']
-					],
-					result:[
-						isActive: true,
-						fields: [
-							Style: [text: "AC-302 - Appointment Card with Heart Sticker"],
-							Color: [text: 'CMYK'],
-							Proof: [text: 'Digital Proof']
-						]
-					]
-				],
+	static def fixtures = [
+		[
+			isActive: true,
+			inputs: [
+				url: "/products/appointment-label-cards-prices.html",
+				title: "Appointment Label Cards Price Quoter",
+				values: [
+					style: [name: '147[]', value: 7462],	//AC-302
+					proof: [name: '64[]', value: '482'],	//online
+					quantity: [name: 'qty[]', value: '500']
+				]
 			],
-		],
-	];	
+			results: [
+				[
+					preAction: {},
+					gleaner: {},
+					values: [
+						Style: "AC-302 - Appointment Card with Heart Sticker",
+						Color: 'CMYK',
+						Proof: 'Digital Proof'
+					]],
+			]
+		]
+	]
 }
+
+
